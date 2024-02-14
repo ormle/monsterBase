26acdb8f1699b7c62accd5ea414cd551f80fd8b0
ormle <101660382+ormle@users.noreply.github.com>
2024-02-14 15:17:20 -0700

    connect render to mongodb atlas

--- database/database.js
+++ database/database.js
@@ -1,19 +1,29 @@
 import mongoose from "mongoose";
     mongoose.set('strictQuery', true);
 
-const uri = "mongodb+srv://315:s0szUcdXdn782rIO@monsters.3fce8hk.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://romel178:Z7qgMOs4x1aSFvN9@cluster0.jeduox4.mongodb.net/?retryWrites=true&w=majority";
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
 export const connectDB = async () => {
-  try {
+    try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
     await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+    } finally {
     // Ensures that the client will close when you finish/error
     await mongoose.disconnect();
-  }
+    }
 }
-//run().catch(console.dir);
+
+/*export const connectDB = async () => {
+  const url = `mongodb://localhost:27017/monsters`;
+  try {
+    const connection = await mongoose.connect(url, {
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected successfully");
+  } catch (e) {
+    console.log("Failed to connect database:", e);
+  }
+};*/
 
