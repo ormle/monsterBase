9300b107c6589c2ad001ad79ab44d293e1c9cb20
ormle <101660382+ormle@users.noreply.github.com>
2024-02-14 15:51:54 -0700

    fix render-mongodb atlas connection

--- database/database.js
+++ database/database.js
@@ -2,16 +2,15 @@ import mongoose from "mongoose";
     mongoose.set('strictQuery', true);
 
 const uri = "mongodb+srv://romel178:Z7qgMOs4x1aSFvN9@cluster0.jeduox4.mongodb.net/?retryWrites=true&w=majority";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
 export const connectDB = async () => {
     try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true} );
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
+    } catch (err) {
+        console.error("Error connecting to MongoDB Atlas!");
     }
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import monsters from "./routes/monster.route.js";
-import axios from "axios";
+
 import { connectDB } from "./database/database.js";
 
 const app = express();
-const port = 8000;
-const monsterURL = "https://gist.githubusercontent.com/mrchenliang/e438f666d121261b74abcd70a5f938d8/raw/a8f14ee5097fe2ab4f78798307d2dd3dcb0dcd3a/monsters.json";
-
+const port = process.env.PORT || 2000;
 
 connectDB();
 
@@ -21,3 +19,4 @@ app.use("/monsters", monsters);
 app.listen(port, function () {
     console.log(`App listening on port ${port}!`);
 })
+
