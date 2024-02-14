4a6f19391e1a83d1616f0d6f4bbb13bfc8843b05
ormle <101660382+ormle@users.noreply.github.com>
2024-02-14 12:59:44 -0700

    Make render connect to mongoDB attempt

--- database/database.js
+++ database/database.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
     mongoose.set('strictQuery', true);
 
-export const connectDB = async () => {
-    const url = `mongodb://localhost:27017/monsters`;
-    try {
-        const connection = await mongoose.connect(url, {
-            useUnifiedTopology: true,
-        });
-        console.log("Database connected successfully");
-    }catch (e) {
-        console.log("Failed to connect database", e);
-    }
+const uri = "mongodb+srv://monsters.3fce8hk.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
+const credentials = "<path_to_certificate>";
+const clientOptions = {
+  tlsCertificateKeyFile: credentials,
+  serverApi: { version: '1', strict: true, deprecationErrors: true }
+};
+export const connectDB = async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
 }
+//run().catch(console.dir);
+
